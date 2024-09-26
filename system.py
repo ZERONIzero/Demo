63b6eae7fc38e9446de0a7ead0a8bab90cdfd7c0
ZeroNi <130401304+ZERONIzero@users.noreply.github.com>
2024-09-26 17:40:39 +0300

    Add files via upload

--- system.py
+++ system.py
@@ -4,18 +4,21 @@ import scipy.spatial.distance as dist
 from imutils import face_utils
 import numpy as np
 
-from classes import blink_class
-from classes import orientation_class
-from classes import liveness_class
-from classes import count_class
+from classes import BlinkEyes_class
+# from classes import orientation_class
+from classes import LiveDetect_class
+from classes import CountFace_class
+from classes import ObjectDetect_class
 
 from imutils.video import VideoStream
 import time
+
 
 cap = VideoStream(src=1).start()
 
 real_detected_time = 0
 penalty_time = 0
+blink_time = 0
 
 type_detect=""
 procent_detect=0
@@ -24,10 +27,11 @@ count_faces = 0
 count_frame=0
 total=0
 
-test1=liveness_class.liveness_detection()
-test2=blink_class.eye_blink_detector()
-test3=orientation_class.face_orientation()
-test_bonus = count_class.count_faces()
+test1=LiveDetect_class.liveness_detection()
+test2=BlinkEyes_class.eye_blink_detector()
+# test3=orientation_class.face_orientation()
+test_bonus1 = CountFace_class.count_faces()
+test_bonus2 = ObjectDetect_class.object_detect()
 
 step1=True
 step2=False
@@ -44,16 +48,21 @@ while True:
     img = cv2.flip(img,1)
     img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-    count_first = test_bonus.count_the_number_of_faces_haarcascade(img_gray)
-    count_second = test_bonus.count_the_number_of_faces_dlib(img_gray)
+    count_first = test_bonus1.count_the_number_of_faces_haarcascade(img_gray)
+    count_second = test_bonus1.count_the_number_of_faces_dlib(img_gray)
 
+    # cell_phone = test_bonus2.cell_phone_detector_second(img)
+    cell_phone1 = test_bonus2.cell_phone_detector(img)
+
+    # print(count_first, count_second)
     if (count_first >= 2 and count_second >=2) or ((count_first == 0 and count_second == 0)):
         step1 = False
         step2 = False
         step3 = False
-        penalty_time = time.time()
-    elif ((time.time() - penalty_time) > 3.0) and real_detect==False:
-        print("Time has run out")
+        penalty_time = round(time.time())
+        if cell_phone1:
+            print("найден телефон")
+    elif (cell_phone1==False) and ((round(time.time()) - penalty_time) > 5.0) and real_detect==False:
         step1 = True
 
     if step1:
@@ -66,8 +75,8 @@ while True:
             elif (time.time() - real_detected_time) > 5.0:
                 step1 = False
                 step2 = True
-                real_detect = True
                 print("real face")
+                real_detect = True
         elif (type_detect == "spoof"):
             print("spoof face")
             real_detected_time = 0
@@ -77,18 +86,19 @@ while True:
             print("unknown face")
             real_detected_time = 0
 
-    if step2:
-        step1 = False
-        step3 = False
-        faces = test2.detector(img_gray)
-        for face in faces:
-            count_frame,total= test2.eye_blink(img_gray,face,count_frame,total)
-            if total >= 4:
-                print("blink detect")
-                step2=False
-                step3=True
-                total=0
-                count_frame=0
+    # if step2:
+    #     step1 = False
+    #     step3 = False
+    #     faces = test2.detector(img_gray)
+    #     for face in faces:
+    #         count_frame,total= test2.eye_blink(img_gray,face,count_frame,total)
+    #         print(total)
+    #         if total >= 4:
+    #             print("blink detect")
+    #             step2=False
+    #             step3=True
+    #             total=0
+    #             count_frame=0
     
 
     # if next1:
@@ -105,7 +115,3 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows() 
-
-# Первым идёт проверка на блики
-# Вторым моргание
-# Третьем повороты головы
